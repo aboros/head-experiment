07f6fddf50a56a5d2f4492cd5283bccbfea06702
Adam Boros <hunaboros@gmail.com>
2016-07-14 17:59:20 +0200

    More fine tuned proof :)

--- script.js
+++ script.js
@@ -10,18 +10,18 @@ function smile() {
     translateY: "+=2",
     translateX: "+=15"
   }, {
-    duration: 200,
+    duration: 900,
     easing: "ease-out"
   })
-  .velocity('reverse', {delay: 400});
+  .velocity('reverse', {delay: 400, duration: 400});
   $('#moustache-right')
   .velocity({
     rotateZ: "-5.5deg",
   }, {
-    duration: 200,
+    duration: 900,
     easing: "ease-out"
   })
-  .velocity('reverse', {delay: 400});
+  .velocity('reverse', {delay: 400, duration: 400});
 }
 
 function eyebrowsDown() {
@@ -33,7 +33,7 @@ function eyebrowsDown() {
     duration: 100,
     easing: "ease-in-out"
   })
-  .velocity('reverse', {delay: 1000, duration: 200});
+  .velocity('reverse', {delay: 1100, duration: 500});
   $('#eyebrow-right .eyebrow-outer')
   .velocity({
     rotateZ: "-15deg",
@@ -42,13 +42,14 @@ function eyebrowsDown() {
     duration: 100,
     easing: "ease-in-out"
   })
-  .velocity('reverse', {delay: 1000, duration: 200});
+  .velocity('reverse', {delay: 1100, duration: 500});
   $('#eyebrow-right')
   .velocity({
+    translateX: "+=40px",
     translateY: "+=80px",
-    rotateZ: "-2deg"
+    rotateZ: "-8deg"
   }, {
     duration: 200
   })
-  .velocity('reverse', {delay: 1000});
+  .velocity('reverse', {delay: 1000, duration: 600});
 }
