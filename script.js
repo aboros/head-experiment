570e0a938c37f850028c6321f6d16901594a3ac5
Adam Boros <hunaboros@gmail.com>
2016-07-14 17:38:12 +0200

    More proof of the concept.

--- script.js
+++ script.js
@@ -1,6 +1,54 @@
 $('#drupglass').click(function() {
-  console.log('drupglass clicked');
-  $('#eyebrow-left')
-  .velocity({rotateZ: "-30deg", transformOriginX: "50%", transformOriginY: "50%"}, {duration: 200, easing: "ease-out"})
-  .velocity('reverse', {delay: 400});
+  smile();
+  eyebrowsDown();
 });
+
+function smile() {
+  $('#moustache-left')
+  .velocity({
+    rotateZ: "4deg",
+    translateY: "+=2",
+    translateX: "+=15"
+  }, {
+    duration: 200,
+    easing: "ease-out"
+  })
+  .velocity('reverse', {delay: 400});
+  $('#moustache-right')
+  .velocity({
+    rotateZ: "-5.5deg",
+  }, {
+    duration: 200,
+    easing: "ease-out"
+  })
+  .velocity('reverse', {delay: 400});
+}
+
+function eyebrowsDown() {
+  $('#eyebrow-right .eyebrow-inner')
+  .velocity({
+    rotateZ: "35deg",
+    translateX: "+=40px"
+  }, {
+    duration: 100,
+    easing: "ease-in-out"
+  })
+  .velocity('reverse', {delay: 1000, duration: 200});
+  $('#eyebrow-right .eyebrow-outer')
+  .velocity({
+    rotateZ: "-15deg",
+    translateX: "-=40px",
+  }, {
+    duration: 100,
+    easing: "ease-in-out"
+  })
+  .velocity('reverse', {delay: 1000, duration: 200});
+  $('#eyebrow-right')
+  .velocity({
+    translateY: "+=80px",
+    rotateZ: "-2deg"
+  }, {
+    duration: 200
+  })
+  .velocity('reverse', {delay: 1000});
+}
