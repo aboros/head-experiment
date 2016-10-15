a8a02946caebd04541eb11981fb4bbd443d40899
Adam Boros <hunaboros@gmail.com>
2016-10-15 12:51:08 +0200

    OK, finally it starts to make sense to me. :)

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ $('#drupglass').click(function() {
   eyebrowsDown();
 });
 
+// Do a "smile" with velocityjs
 function smile() {
   $('#moustache-left')
   .velocity({
@@ -24,6 +25,7 @@ function smile() {
   .velocity('reverse', {delay: 400, duration: 400});
 }
 
+// Move eyebrows down with velocityjs
 function eyebrowsDown() {
   $('#eyebrow-right .eyebrow-inner')
   .velocity({
@@ -54,19 +56,37 @@ function eyebrowsDown() {
   .velocity('reverse', {delay: 1000, duration: 600});
 }
 
+// Try some mimic with gsap
+// Superbasic eyebrow rotation
+var tween = new TweenMax.to('#eyebrow-right .eyebrow-inner', 0.5, {rotation:"35deg", x: "+=40px", transformOrigin: "right top"});
+
+// Combine multiple things into one object
+var eyeBrowRightTimeline = new TimelineMax({});
+eyeBrowRightTimeline.add(tween, 0);
+eyeBrowRightTimeline.add(TweenMax.to("#eyebrow-right .eyebrow-outer", 0.5, {rotation: "-15deg", x: "-=40px", transformOrigin: "left top"}), 0);
+eyeBrowRightTimeline.add(TweenMax.to("#eyebrow-right", 0.5, {rotation: "-8deg", x: "+=40px", y: "+=80px",transformOrigin: "center center"}), 0);
+
 // ScrollMagic fun.
 // init controller
 var controller = new ScrollMagic.Controller();
 
 // setup pin
+
+/* setPin is buggy on iOS safari for some reason. Examples page produces same bug, so hm.. dunno.
+
 var pin = new ScrollMagic.Scene({triggerElement: "#screen-1", triggerHook: 0.85, duration: "150%"})
 .setPin("#logo")
 .addIndicators({name: "pin logo"}) // add indicators (requires plugin)
 .addTo(controller);
+*/
 
-// setup zoom
-var zoom = new ScrollMagic.Scene({triggerElement: "#screen-2", triggerHook: "onEnter"})
-  // .setTween(tween)
-  .setVelocity("#logo", {rotateZ: "6deg"}, {duration: 200})
+// Setup a zoom on logo
+var zoom = new ScrollMagic.Scene({triggerElement: "#screen-2", offset: "50px"})
+  .setTween("#logo", 0.5, {scale: 1.4, rotation: "12deg"})
   .addIndicators({name: "zoom logo"}) // add indicators (requires plugin)
+  .addTo(controller);
+
+var gsapTest = new ScrollMagic.Scene({triggerElement: "#screen-2", duration: 50})
+  .setTween(eyeBrowRightTimeline)
+  .addIndicators({name: "gsapTest yeah"})
   .addTo(controller);
